594b31f38f3fb32132583df98667cacb9f129e23
Raunit Verma <ramgopal10258@gmail.com>
2021-07-30 17:40:57 +0530

    Major changes in Page
    Added the Contact Support in the page

--- shypt.js
+++ shypt.js
@@ -77,3 +77,12 @@ close_btn.addEventListener('click',()=>{
         nav_el.classList.remove('visible')
     });
 })
+
+const labels=document.querySelectorAll('.form-control label')
+
+labels.forEach(label=>{
+    label.innerHTML=label.innerText
+    .split('')
+    .map((letter,idx)=>`<span style="transition-delay:${idx*30}ms" >${letter}</span>`)
+    .join('')
+})
